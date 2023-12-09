680bb87b69a9161f55004069d6c243ee229dd4c3
suyeon <sroovy@naver.com>
2023-12-09 21:07:41 +0900

    feat | post 전체보기 페이지 추가

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,42 @@
-function AllPostPage() {
+import AllPosts from "../../components/posts/AllPosts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2023-12-09",
+  },
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2023-12-09",
+  },
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2023-12-09",
+  },
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2023-12-09",
+  },
+];
+
+function AllPostPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
 export default AllPostPage;
