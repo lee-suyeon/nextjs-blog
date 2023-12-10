fa019fefedaa5918e0b074a7ea5767b2979ed12c
suyeon <sroovy@naver.com>
2023-12-10 16:18:00 +0900

    feat | 개별 포스트 페이지, 경로 렌더링

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -3,22 +3,14 @@ import PostHeader from "./PostHeader";
 
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-nextjs",
-  title: "Getting started with NextJs",
-  image: "getting-started-nextjs.png",
-  date: "2023-12-09",
-  content: "# This is a first post",
-};
-
-function PostContent() {
-  const { slug, image, title } = DUMMY_POST;
+function PostContent({ post }) {
+  const { slug, image, title } = post;
   const imagePath = `/images/posts/${slug}/${image}`;
 
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
