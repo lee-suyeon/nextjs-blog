cbde665b21bb0e5db752455b6f75e2fcd1eaf955
suyeon <sroovy@naver.com>
2023-12-10 20:29:48 +0900

    feat | 마크다운 코드 템플릿 렌더링

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./PostHeader";
 import classes from "./post-content.module.css";
@@ -30,6 +32,18 @@ function PostContent({ post }) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
