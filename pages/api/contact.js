174f0f1c06b9153f8419cfde4ea8ef2adea586d3
suyeon <sroovy@naver.com>
2023-12-11 21:17:39 +0900

    feat | contact 메세지 db에 저장하기

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,10 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      email.includes("@") ||
+      !email.includes("@") ||
       !name ||
       name.trim() === "" ||
       !message ||
@@ -21,7 +23,32 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://sroovy:HqTV6J6N2TXDisTZ@cluster0.3pvlfkr.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({
+        message: "Could not connect to database",
+      });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({
+        message: "Storing message failed!",
+      });
+      return;
+    }
+
+    client.close();
+
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
