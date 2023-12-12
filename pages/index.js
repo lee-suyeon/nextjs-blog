5a7d334b35eeda25cb1d40bc125bd5e14b38060b
suyeon <sroovy@naver.com>
2023-12-12 20:53:53 +0900

    feat | head 데이터 추가

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Head from "next/head";
+
 import Hero from "../components/Homepage/Hero";
 import FeaturedPosts from "../components/Homepage/FeaturedPosts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -6,6 +8,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage({ posts }) {
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to my blog!</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,21 @@
+import { Fragment } from "react";
 import AllPosts from "../../components/posts/AllPosts";
 import { getAllPosts } from "../../lib/posts-util";
+import Head from "next/head";
 
 function AllPostPage({ posts }) {
-  return <AllPosts posts={posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="A list of all programming-related tutorials and posts!"
+        />
+      </Head>
+      <AllPosts posts={posts} />;
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
