41cf0d1ec25bcc430395283bc2346f03477e2169
suyeon <sroovy@naver.com>
2023-12-15 22:16:18 +0900

    feat | 가입 요청 전송하기

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,32 +2,33 @@ import { connectToDatabase } from "../../lib/db";
 import { hashPassword } from "../../lib/auth";
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
+    const { email, password } = data;
 
-  const { email, password } = data;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({ message: "Invalid input." });
-    return;
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created user!" });
   }
-
-  const client = await connectToDatabase();
-
-  const db = client.db();
-
-  const hashedPassword = await hashPassword(password);
-
-  const result = await db.collection("users").insertOne({
-    email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
